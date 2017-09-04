63fa8fa38502fbcaf6d2547b479e85332b89fcea
Edward Seim <sirseim@gmail.com>
2017-09-04 13:49:02 -0700

    Clean up code with ESLint linter

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
 #!/usr/bin/env node
-const argv = require('yargs')
+const argv = require('yargs') // eslint-disable-line prefer-destructuring
   .usage('Usage: $0 <file> [options]')
+  .describe('v', 'Show Info')
+  .describe('vv', 'Show Info & Debug')
+  .help('h')
   .demandCommand(1)
   .count('verbose')
   .alias('v', 'verbose')
   .argv;
-const csv = require("fast-csv");
-const moment = require("moment");
+const csv = require('fast-csv');
+const moment = require('moment');
 
 const VERBOSE_LEVEL = argv.verbose;
-const WARN = (args) => VERBOSE_LEVEL >= 0 && console.log(args);
-const INFO = (args) => VERBOSE_LEVEL >= 1 && console.log(args);
-const DEBUG = (args) => VERBOSE_LEVEL >= 2 && console.log(args);
+const WARN = args => VERBOSE_LEVEL >= 0 && console.log(args); // eslint-disable-line no-unused-vars
+const INFO = args => VERBOSE_LEVEL >= 1 && console.log(args); // eslint-disable-line no-unused-vars
+const DEBUG = args => VERBOSE_LEVEL >= 2 && console.log(args); // eslint-disable-line no-unused-vars
 
 const file = argv._[0];
 INFO(`File to be read: ${file}`);
 
 const datesToPeople = [];
 const STARTDATE = moment.unix(1451606400);
-var previousDay = -1;
-
-const addPersonToData = (data, person, day) => {
-  if (!data[day]) {
-    data[day] = {};
-  }
-
-  if (!data[day][person]) {
-    data[day][person] = 1;
-
-    var dayCounter = day - 1;
-    while (dayCounter >= 0 && data[dayCounter] && data[dayCounter][person]) {
-      data[dayCounter][person]++;
-      dayCounter--;
-    }
-  }
-};
+let previousDay = -1;
 
 csv
   .fromPath(file)
-  .on("data", (data) => {
-    const datetime = moment.unix(parseInt(data[0]));
+  .on('data', (data) => {
+    const datetime = moment.unix(parseInt(data[0], 10));
     const user = data[1];
 
     const day = datetime.diff(STARTDATE, 'days');
@@ -48,32 +35,49 @@ csv
       previousDay = day;
     }
 
-    addPersonToData(datesToPeople, user, day);
+    if (!datesToPeople[day]) {
+      datesToPeople[day] = {};
+    }
+
+    if (!datesToPeople[day][user]) {
+      datesToPeople[day][user] = 1;
+
+      // go back and add to previous consecutive days
+      let dayCounter = day - 1;
+      while (dayCounter >= 0 && datesToPeople[dayCounter] && datesToPeople[dayCounter][user]) {
+        datesToPeople[dayCounter][user] += 1;
+        dayCounter -= 1;
+      }
+    }
   })
-  .on("end", () => {
+  .on('end', () => {
     INFO('Done reading CSV');
     datesToPeople.forEach((day, index) => {
       DEBUG(`Processing day: ${index}`);
+      DEBUG(`People in day: ${Object.keys(day).length}`);
+
       const usersConsecutive = [];
+
       Object.keys(day).forEach((user) => {
         if (!usersConsecutive[day[user] - 1]) {
           usersConsecutive[day[user] - 1] = 1;
         } else {
-          usersConsecutive[day[user] - 1]++;
+          usersConsecutive[day[user] - 1] += 1;
         }
       });
-      DEBUG(`People in day: ${Object.keys(day).length}`);
 
-      var output = `${index + 1},`;
+      let output = `${index + 1},`;
       const LAST_INDEX = 13;
-      for (var consecutivePlayIndex = 0; consecutivePlayIndex < 14; consecutivePlayIndex++) {
-        var consecutive = usersConsecutive[consecutivePlayIndex];
+
+      for (let consecutivePlayIndex = 0; consecutivePlayIndex < 14; consecutivePlayIndex += 1) {
+        const consecutive = usersConsecutive[consecutivePlayIndex];
         if (consecutivePlayIndex === LAST_INDEX) {
           output += `${consecutive || 0}`;
         } else {
           output += `${consecutive || 0},`;
         }
       }
+
       console.log(output);
     });
   });
